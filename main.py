8297c56d656da88ef925f2cf61b2c254425c1dc9
Victor Marcias <victor.marcias@gmail.com>
2026-07-08 15:54:42 -0300

    Create fastapi-blog branch

---
+++ main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Victor Marcias",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI makes it even better.",
+        "date_posted": "April 21, 2025",
+    },
+]
+
+
+########################################
+########################################
+### API ###
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(request: Request, post_id: int):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Papá no aparece")
+
+
+########################################
+########################################
+### CONTENT
+
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request, 
+        "home.html", 
+        {"posts": posts, "title": "PAGINA PRENCEPAL"},
+    )
+
+@app.get("/posts/{post_id}", include_in_schema=False)
+def post_page(request: Request, post_id: int):
+    for post in posts:
+        if post["id"] == post_id:
+            title = post["title"][:50]
+            return templates.TemplateResponse(
+                request,
+                "post.html", 
+                {"post": post, "title": title},
+            )
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="Papá no aparece"
+    )
