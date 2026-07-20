9bdc935ae7158644c258f5ef1443ed054d815931
Victor Marcias <victor.marcias@gmail.com>
2026-07-20 17:43:14 -0300

    Part 4 - Pydantic schemas, POST

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
-
+##
+from schemas import PostCreate, PostResponse
+##
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -39,17 +41,36 @@ posts: list[dict] = [
 ########################################
 ### API ###
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(request: Request, post_id: int):
     for post in posts:
         if post["id"] == post_id:
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Papá no aparece")
 
+########################################
+########################################
+## Create Post
+@app.post(
+    "/api/posts",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025",
+    }
+    posts.append(new_post)
+    return new_post
 
 ########################################
 ########################################
